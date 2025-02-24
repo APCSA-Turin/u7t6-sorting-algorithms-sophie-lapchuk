7bee836f1c62626ee605c037d698dfebafb4009c
Sophie Lapchuk <sophiel40@nycstudents.net>
2025-02-24 23:11:04 +0000

    first commit

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,14 +8,33 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-       
+     
+        for (int i = 0; i < elements.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < elements.length; j++) {
+                if (elements[j] < elements[min]) {
+                    int temp = elements[j];
+                    elements[j] = elements[min];
+                    elements[min] = temp;
+                }
+            }
+        }
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-      
+        for (int i = 0; i < words.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < words.size(); j++) {
+                if (words.get(j).compareTo(words.get(minIndex))< 0) {
+                    String temp = words.get(j);
+                    words.set(j, words.get(minIndex));
+                    words.set(minIndex, temp);
+                }
+            }
+        }
         return words;
     }
 
@@ -31,5 +50,9 @@ public class SelectionSort {
             System.out.println(e.getMessage());
         }
         return wordList;
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
